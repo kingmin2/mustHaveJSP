915d082614d034eaafc30de7b0223b73887e5f78
kingmin2 <meanjeong92@gmail.com>
2022-03-30 20:50:51 +0900

    5장 JDBC

---
+++ web_stand/src/main/java/common/JDBConnect.java
@@ -0,0 +1,63 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBConnect {
+	
+	public Connection conn;
+	public Statement stmt;
+	public PreparedStatement pstmt;
+	public ResultSet rs;
+	
+	//기본 생성자
+	public JDBConnect() {
+	
+		try {
+			//JDBC 드라이버 로드
+			Class.forName("oracle.jdbc.OracleDriver");
+			
+			//DB연결
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "scott", "tiger");
+			
+			System.out.println("DB 연결 성공(기본생성자)");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+	}
+	
+	public void close() {
+		try {
+			if(rs != null) rs.close();
+			if(stmt != null) stmt.close();
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+			System.out.println("JDBC 자원 해제");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+ 
+	public JDBConnect(String driver, String url, String id, String pwd) {
+		
+		try {
+		
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, id, pwd);
+			System.out.println("DB 연결 성공 (인수 생성자 1)");
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+
+}
---
+++ web_stand/src/main/webapp/05JDBC/ConnectionTest.jsp
@@ -0,0 +1,27 @@
+<%@page import="common.JDBConnect"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>JDBC</title>
+</head>
+<body>
+	<h2>JDBC테스트 1</h2>
+	<%
+	JDBConnect jdbc1 = new JDBConnect();
+	jdbc1.close();
+	%>
+	
+	<h2>JDBC테스트 2</h2>
+	<%
+	String driver = application.getInitParameter("OracleDriver");
+	String url    = application.getInitParameter("OracleURL");
+	String id     = application.getInitParameter("OracleId");
+	String pwd    = application.getInitParameter("OraclePwd");
+	JDBConnect jdbc2 = new JDBConnect(driver, url, id, pwd);
+	jdbc2.close();
+	%>
+</body>
+</html>
